b8358d31b19989bf437157de3096f4498ee26970
takliu <liuatk999@gmail.com>
2021-06-26 13:06:46 +0800

    Update vocab.py
    1.  finish download function

--- vocab.py
+++ vocab.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-
-import sys, getopt, urllib
+from tqdm import tqdm
+import sys, getopt, requests, cgi
 
 MA3_DOWNLOAD_URL = "https://ssl.gstatic.com/dictionary/static/sounds/oxford/%s--_gb_1.mp3"
+buffer_size = 1024
 
 
 def main(argv):
@@ -19,13 +20,31 @@ def main(argv):
         sys.exit()
       elif opt in ("-d", "--vocab"):
         vocabulary = arg
-   		print('Vocabulary is ', vocabulary)
+        downloadMp3(vocabulary)
 
 def downloadMp3(vocab):
-	   		
+   downloadURL = MA3_DOWNLOAD_URL % vocab
+   response = requests.get(downloadURL, stream=True)
+   file_size = int(response.headers.get("Content-Length", 0))
+   content_disposition = response.headers.get("Content-Disposition")
+
+   default_filename = "%s.mp3" % vocab 
+   
+   if content_disposition:
+      value, params = cgi.parse_header(content_disposition)
+      filename = params.get("filename", default_filename)
+   else:
+      filename = default_filename
+
+   progress = tqdm(response.iter_content(buffer_size), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
+   with open(filename, "wb") as f:
+      for data in progress.iterable:
+         f.write(data)
+         progress.update(len(data))
+   	   	
 
 def showInstruction():
-	print('test.py -d <vocabulary>')
+	print('vocab.py -d <vocabulary>')
 
 
 if __name__ == "__main__":
